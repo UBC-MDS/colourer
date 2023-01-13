d3e62d52e197b7ead37ceedca6c9203a796ef91b
Lauren Zung <59011010+lzung@users.noreply.github.com>
2023-01-12 17:14:57 -0800

    minor edits

--- src/colourpycker/colourpycker.py
+++ src/colourpycker/colourpycker.py
@@ -4,7 +4,6 @@
 
 
 def donut(img_url, num_clrs, img_size):
-    
     """Create a donut chart of the top n colors in the image (number of colors specified by the user)
     
     Creates a square donut chart using the n most common colors from the image 
@@ -13,17 +12,17 @@ def donut(img_url, num_clrs, img_size):
     ----------
     img_url: str
         the url of the image that the user is pulling the colors from
-    num_clrs: integer
+    num_clrs: int
         the number of colors the user wants to pull from the image
-    img_size: integer
+    img_size: int
         the pixel width and height of the resulting chart
     
     Returns
     ----------
-    colourpycker.donut
-        Altair donut chart, specifically an altair.vegalite.v4.api.Chart
+    altair.vegalite.v4.api.Chart
+        Donut chart of the colours
     
     Examples
     --------
-    donut(https://visit.ubc.ca/wp-content/uploads/2019/04/plantrip_header-2800x1000_2x.jpg, 5, 400)
+    donut('https://visit.ubc.ca/wp-content/uploads/2019/04/plantrip_header-2800x1000_2x.jpg', 5, 400)
     """
