2992cbb3b3a910de85f2302433b586af9d370d42
Arjun Radhakrishnan <rkrishnan.arjun@gmail.com>
2023-01-19 16:00:22 -0800

    Improved comments

--- tests/test_colourpycker.py
+++ tests/test_colourpycker.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 def test_get_color_palette_valid_input():
-    """Test when input parameters are valid"""
+    """Test for get_color_palette when input parameters are valid"""
     img_url = "https://visit.ubc.ca/wp-content/uploads/2019/04/plantrip_header-2800x1000_2x.jpg"
     tolerance = 20
     limit = 5
@@ -13,7 +13,7 @@ def test_get_color_palette_valid_input():
 
 
 def test_get_color_palette_not_a_url():
-    """Test when URL is a random string"""
+    """Test for get_color_palette when URL is a random string"""
     img_url = "soijfoidjfosdijfoisdj"
     tolerance = 20
     limit = 5
@@ -23,7 +23,7 @@ def test_get_color_palette_not_a_url():
 
 
 def test_get_color_palette_url_but_not_image():
-    """Test when URL is valid but it is not pointing to an image"""
+    """Test for get_color_palette when URL is valid but it is not pointing to an image"""
     img_url = "https://soijfoidjfosdijfoisdj.url"
     tolerance = 20
     limit = 5
@@ -33,8 +33,8 @@ def test_get_color_palette_url_but_not_image():
 
 
 def test_get_color_palette_invalid_tolerance():
-    """Test when tolerance is invalid"""
-    img_url = "https://soijfoidjfosdijfoisdj.url"
+    """Test for get_color_palette when tolerance is invalid"""
+    img_url = "https://visit.ubc.ca/wp-content/uploads/2019/04/plantrip_header-2800x1000_2x.jpg"
     tolerance = -20000
     limit = 5
     result = colourpycker.get_color_palette(img_url, tolerance, limit)
